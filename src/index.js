eaab77e1aa88f172f37f1f66dbe49e3e3e6b5d1b
Caleb Peterson <caleb.peterson@cubicle6.com>
2020-04-20 18:56:32 -0400

    working prototype

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const {
+  app,
+  clipboard,
+  globalShortcut,
+  screen,
+  BrowserWindow,
+  Menu,
+} = require("electron");
+
+const path = require("path");
+
+require("electron-reload")(path.join(__dirname, "src"), {
+  electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+});
+
+const isDevelopment = process.env.NODE_ENV === "development";
+const isProduction = !isDevelopment;
+
+const SHORTCUT = "Alt+X";
+
+function initialize() {
+  globalShortcut.register(SHORTCUT, () => {
+    const win = new BrowserWindow({
+      center: true,
+      width: 600,
+      height: 400,
+      frame: false,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    });
+
+    win.loadFile("index.html");
+
+    if (isDevelopment) {
+      win.webContents.openDevTools({ mode: "detach" });
+    }
+    win.focus();
+    win.on("blur", () => {
+      if (!win.webContents.isDevToolsFocused()) {
+        win.close();
+      }
+    });
+  });
+}
+
+app.whenReady().then(initialize);
+
+app.on("window-all-closed", (e) => e.preventDefault());
+
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll();
+});
