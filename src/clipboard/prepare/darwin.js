1fe1480d9e860c949f89323400f5668a6d5b11a4
Caleb Peterson <caleb.peterson@zapier.com>
2023-11-22 09:31:15 -0500

    update .nvmrc to use v18

--- src/clipboard/prepare/darwin.js
+++ src/clipboard/prepare/darwin.js
@@ -1,8 +1,35 @@
 const runApplescript = require("run-applescript");
+const { exec } = require("child_process");
+const _ = require("lodash");
 
-const script = `
-tell application "System Events"
-   keystroke "c" using command down
-end tell
-`;
-module.exports = () => runApplescript(script);
+const OSASCRIPT_PATH = "/usr/bin/osascript";
+
+const osascript = (script, callback = _.noop) =>
+  new Promise((resolve, reject) => {
+    const cmd = `${OSASCRIPT_PATH} <<EOF\n${script}\nEOF`;
+    exec(cmd, (error, stdout, stderr) => {
+      if (stdout) {
+        const query = _.trim(stdout);
+        const rawQueryCommand = {
+          isUnhandled: true,
+          query,
+        };
+        const command =
+          commands.find(
+            ({ title, isFallback }) => title === query && !isFallback
+          ) || rawQueryCommand;
+        resolve(command);
+      } else {
+        if (error) {
+          console.error(error);
+        }
+        resolve({
+          isUnknown: true,
+        });
+      }
+    });
+  });
+
+const script = `tell application "System Events" to keystroke "c" using {command down}`;
+
+module.exports = () => osascript(script);
