afb3e43b51b5e57f715326c7283f3e38f2447cee
Caleb Peterson <caleb.peterson@zapier.com>
2023-12-06 15:24:29 -0500

    limit the width of the macOS choose-gui command picker

--- src/terminal/prompt/darwin.mjs
+++ src/terminal/prompt/darwin.mjs
@@ -1,5 +1,5 @@
-import { exec } from 'child_process';
-import _ from 'lodash';
+import { exec } from "child_process";
+import _ from "lodash";
 
 // Uses choose: https://github.com/chipsenkbeil/choose
 // brew install choose-gui
@@ -7,7 +7,7 @@ export default (commands) =>
   new Promise((resolve, reject) => {
     const choices = commands.map(({ title }) => title).join("\n");
     const toShow = Math.min(20, _.size(commands));
-    const cmd = `echo "${choices}" | choose -b 000000 -c 222222 -s 18 -m -n ${toShow}`;
+    const cmd = `echo "${choices}" | choose -b 000000 -c 222222 -w 30 -s 18 -m -n ${toShow}`;
     exec(cmd, (error, stdout, stderr) => {
       if (stdout) {
         const query = _.trim(stdout);
