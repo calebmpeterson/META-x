8cc49d39a07fa96e60384a151ad859c92766f64a
caleb <caleb.peterson@cubicle6.com>
2023-12-20 20:29:36 -0500

    increase font size

--- bin/launcher.mjs
+++ bin/launcher.mjs
@@ -174,7 +174,7 @@ var promptDarwin = (commands) =>
   new Promise((resolve, reject) => {
     const choices = commands.map(({ title }) => title).join("\n");
     const toShow = Math.min(20, _.size(commands));
-    const cmd = `echo "${choices}" | choose -b 000000 -c 222222 -w 30 -s 16 -m -n ${toShow}`;
+    const cmd = `echo "${choices}" | choose -b 000000 -c 222222 -w 30 -s 18 -m -n ${toShow}`;
     exec(cmd, (error, stdout, stderr) => {
       if (stdout) {
         const query = _.trim(stdout);
