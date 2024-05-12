c1ca2a5c17ab49e9bed55f9dee5d825f72793c39
caleb <caleb.peterson@cubicle6.com>
2024-05-12 09:20:12 -0400

    show calculation results using quickulator

--- src/utils/showCalculationResultDialog.mjs
+++ src/utils/showCalculationResultDialog.mjs
@@ -1,17 +1,14 @@
 import { execa } from "execa";
+import os from "node:os";
+import path from "node:path";
 
 export const showCalculationResultDialog = async (query, result) => {
-  // await cocoaDialog("msgbox", {
-  //   title: `META-x`,
-  //   text: result,
-  //   timeout: 5,
-  //   timeoutFormat: " ",
-  // });
-  await execa("open", [
-    "-n",
-    "-a",
-    "Brave Browser",
-    "--args",
-    `--app=https://quickulator.cubicle6.com/?code=${encodeURIComponent(query)}`,
-  ]);
+  const cwd = path.join(os.homedir(), "Tools", "quickulator", "app");
+  const target = path.join(cwd, "dist", "quickulator");
+
+  try {
+    await execa(target, [...query], { cwd, preferLocal: true });
+  } catch (error) {
+    console.error(`Failed to show calculation result`, error);
+  }
 };
